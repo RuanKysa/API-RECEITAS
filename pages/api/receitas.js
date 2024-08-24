03c0365ba115e39bcfeb61ccc479938d24d192da
RuanKysa <ruancjbueno@gmail.com>
2024-08-23 21:22:25 -0300

    api

--- pages/api/receitas.js
+++ pages/api/receitas.js
@@ -1,51 +1,60 @@
 import fs from 'fs';
 import path from 'path';
 
-const receitasPath = path.resolve('.', 'data', 'receitas.json');
-const idPath = path.resolve('.', 'data', 'id.json');
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+let receitasPath, idPath;
+if (isDevelopment) {
+    receitasPath = path.resolve('.', 'data', 'receitas.json');
+    idPath = path.resolve('.', 'data', 'id.json');
+}
 
 const ensureFileExists = (filePath, initialData = []) => {
-    if (!fs.existsSync(filePath)) {
+    if (isDevelopment && !fs.existsSync(filePath)) {
         fs.writeFileSync(filePath, JSON.stringify(initialData, null, 2));
     }
 };
 
-ensureFileExists(receitasPath);
-ensureFileExists(idPath, { lastId: "0" });
+if (isDevelopment) {
+    ensureFileExists(receitasPath);
+    ensureFileExists(idPath, { lastId: "0" });
+}
 
 const getNextId = () => {
-    if (fs.existsSync(idPath)) {
+    if (isDevelopment && fs.existsSync(idPath)) {
         const { lastId } = JSON.parse(fs.readFileSync(idPath, 'utf8'));
         const nextId = (parseInt(lastId, 10) + 1).toString();
         fs.writeFileSync(idPath, JSON.stringify({ lastId: nextId }));
         return nextId;
     } else {
-        fs.writeFileSync(idPath, JSON.stringify({ lastId: "0" }));
-        return "1";
+        return null;
     }
 };
 
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
-            const receitas = JSON.parse(fs.readFileSync(receitasPath, 'utf8'));
-            res.status(200).json(receitas);
+            if (isDevelopment) {
+                const receitas = JSON.parse(fs.readFileSync(receitasPath, 'utf8'));
+                res.status(200).json(receitas);
+            } else {
+                res.status(200).json([]);
+            }
         } catch (error) {
             res.status(500).json({ message: 'Erro ao ler o arquivo de receitas' });
         }
     } else if (req.method === 'POST') {
-
         try {
-            const novaReceita = req.body;
-            const receitas = JSON.parse(fs.readFileSync(receitasPath, 'utf8'));
- 
-            novaReceita.id = getNextId();
-            
-
-            receitas.push(novaReceita);
-            fs.writeFileSync(receitasPath, JSON.stringify(receitas, null, 2));
-            
-            res.status(201).json(novaReceita);
+            if (isDevelopment) {
+                const novaReceita = req.body;
+                const receitas = JSON.parse(fs.readFileSync(receitasPath, 'utf8'));
+                novaReceita.id = getNextId();
+                receitas.push(novaReceita);
+                fs.writeFileSync(receitasPath, JSON.stringify(receitas, null, 2));
+                res.status(201).json(novaReceita);
+            } else {
+                res.status(500).json({ message: 'Operação não permitida no ambiente de produção' });
+            }
         } catch (error) {
             res.status(500).json({ message: 'Erro ao adicionar a receita' });
         }
